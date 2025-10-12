df25a2e5b16774378de4630e0c3a1c14465301bb
Yifan-858 <133569248+Yifan-858@users.noreply.github.com>
2025-10-12 23:19:57 +0200

    Added customer manager

---
+++ SimpleShop/Customers/BronzeCustomer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleShop.Customers
+{
+    public class BronzeCustomer : Customer
+    {
+        public BronzeCustomer(string name, string password) : base(name, password, 0.05, "Bronze")
+        {
+        }
+    }
+}
---
+++ SimpleShop/Manager/CustomerManager.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SimpleShop.Customers;
+
+namespace SimpleShop.Manager
+{
+    public static class CustomerManager
+    {
+        private static string filePath = Path.Combine("Data", "customers.txt");
+
+        public static void SaveCustomerToFile(List<Customer> customers)
+        {
+            Directory.CreateDirectory("Data");//for bin/Debug/net8.0 ??
+            List<string> lines = new List<string>();
+
+            foreach(var c in customers)
+            {
+                lines.Add($"{c.Name};{c.GetPassword()};{c.Discount};{c.Level}");
+            }
+
+            File.WriteAllLines(filePath, lines);
+            Console.WriteLine("Customers saved successfully!");
+        }
+
+        public static List<Customer> LoadCustomersFromFile()
+        {
+            List<Customer> customers = new List<Customer>();
+            if (!File.Exists(filePath))
+            {
+                return customers;
+            }
+
+            string[] lines = File.ReadAllLines(filePath);
+            Customer loadedCustomer = null;
+
+            foreach(string l in lines)
+            {
+                string[] parts = l.Split(';');
+                if(parts.Length >= 4)
+                {
+                    string name = parts[0];
+                    string password = parts[1];
+                    string level = parts[3];
+
+                    switch (level)
+                    {
+                        case "Bronze":
+                            loadedCustomer = new BronzeCustomer(name, password);
+                            break;
+                        case "Silver":
+                            loadedCustomer = new SilverCustomer(name, password);
+                            break;
+                        case "Gold":
+                            loadedCustomer = new GoldCustomer(name, password);
+                            break;
+                    }
+
+                    customers.Add(loadedCustomer);
+                }
+            }
+
+            return customers;
+        }
+    }
+}
